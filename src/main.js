e718f61f244cab48b46fcb1eea0fc3e9fa13d029
Dikshit Jineshwar Tibole <dikshitjtibole@gmail.com>
2024-08-21 17:27:41 +0530

    Connect to Database

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+// const app = express()
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+//         app.on("error", (error) => {
+//             console.log("Error:", error);
+//             throw error;            
+//         }
+//         )
+//         app.listen(process.env.PORT, () => { console.log('And All Set...👍') })
+        
+//     } catch (error) {
+//         console.error("Error:", error);
+        
+//     }
+// }
+// )()
