9b090cba16474133d21052dd43abb91e2d6e46aa
Dikshit Jineshwar Tibole <dikshitjtibole@gmail.com>
2024-08-28 20:27:07 +0530

    Use Cloudinary and multer

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary"
+import { error } from "console"
+cloudinary.v2.uploader.upload
+import fs from "fs/promises"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloud = async(localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        console.log("File Upload on Coudinary Sucessfully", response.url);
+        return response;
+        
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null; 
+    }
+}
+
+export{uploadOnCloud}
